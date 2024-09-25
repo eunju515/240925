1cc6251681196de7f040c5a202256bbc46f2c37f
eunju515 <130980334+eunju515@users.noreply.github.com>
2024-09-25 11:15:52 +0900

    Update 날씨검색.py

--- pages/날씨검색.py
+++ pages/날씨검색.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+from urllib.parse import quote
 
 # 웹 애플리케이션 제목
 st.title('오늘의 지역별 날씨 조회')
@@ -9,6 +10,7 @@ api_key = st.text_input('API 키를 입력해주세요:', type="password")
 
 # 지역 입력 받기
 city = st.text_input('날씨를 조회할 도시 이름을 입력해주세요:')
+country_code = st.text_input('국가 코드를 입력해주세요 (예: KR, US 등):')
 
 # 버튼을 눌렀을 때 날씨 정보 조회
 if st.button('날씨 조회하기'):
@@ -17,8 +19,17 @@ if st.button('날씨 조회하기'):
     elif not city:
         st.error('도시 이름을 입력해주세요!')
     else:
+        # 한글 도시 이름을 URL에 맞게 인코딩
+        encoded_city = quote(city)
+        
+        # 국가 코드가 있으면 함께 사용
+        if country_code:
+            city_query = f"{encoded_city},{country_code}"
+        else:
+            city_query = encoded_city
+        
         # OpenWeatherMap API 요청 URL
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&lang=kr&units=metric'
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city_query}&appid={api_key}&lang=kr&units=metric'
 
         # API 호출
         response = requests.get(url)
